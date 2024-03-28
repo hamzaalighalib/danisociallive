1acd63b4a31437d6509b69e1165d7ddf4f626a64
hamzaalighalib <97229996+hamzaalighalib@users.noreply.github.com>
2024-03-28 15:28:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,38 @@ const config = {
   }
 };
 
-var nms = new NodeMediaServer(config)
-nms.run();
+var nms = new NodeMediaServer(config);
+
+nms.on('preConnect', (id, args) => {
+  console.log(`[NodeMediaServer] Client connected: ${id}`);
+});
+
+nms.on('postConnect', (id, args) => {
+  console.log(`[NodeMediaServer] Client connected: ${id}`);
+});
+
+nms.on('doneConnect', (id, args) => {
+  console.log(`[NodeMediaServer] Client disconnected: ${id}`);
+});
+
+nms.on('prePublish', (id, StreamPath, args) => {
+  console.log(`[NodeMediaServer] Stream published: ${StreamPath}`);
+});
+
+nms.on('donePublish', (id, StreamPath, args) => {
+  console.log(`[NodeMediaServer] Stream unpublished: ${StreamPath}`);
+});
+
+nms.on('donePlay', (id, StreamPath, args) => {
+  console.log(`[NodeMediaServer] Stream played: ${StreamPath}`);
+});
+
+nms.on('postPlay', (id, StreamPath, args) => {
+  console.log(`[NodeMediaServer] Stream stopped: ${StreamPath}`);
+});
+
+nms.run(() => {
+  console.log('Node Media Server is running');
+  console.log(`RTMP server URL: rtmp://localhost:${config.rtmp.port}/live`);
+  console.log(`HTTP server URL: http://localhost:${config.http.port}`);
+});
